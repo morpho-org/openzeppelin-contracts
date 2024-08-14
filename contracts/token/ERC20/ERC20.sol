880236c57021f8bc77bb525cc02361bb37fccab4
Adrien Husson <adhusson@gmail.com>
2024-08-14 19:06:11 +0200

    feat: public ERC20 name and symbol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -38,8 +38,8 @@ abstract contract ERC20 is Context, IERC20, IERC20Metadata, IERC20Errors {
 
     uint256 private _totalSupply;
 
-    string private _name;
-    string private _symbol;
+    string public name;
+    string public symbol;
 
     /**
      * @dev Sets the values for {name} and {symbol}.
@@ -48,23 +48,8 @@ abstract contract ERC20 is Context, IERC20, IERC20Metadata, IERC20Errors {
      * construction.
      */
     constructor(string memory name_, string memory symbol_) {
-        _name = name_;
-        _symbol = symbol_;
-    }
-
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() public view virtual returns (string memory) {
-        return _name;
-    }
-
-    /**
-     * @dev Returns the symbol of the token, usually a shorter version of the
-     * name.
-     */
-    function symbol() public view virtual returns (string memory) {
-        return _symbol;
+        name = name_;
+        symbol = symbol_;
     }
 
     /**
